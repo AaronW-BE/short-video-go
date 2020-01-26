61ed0232bcd36ef34613115130989e0756a41e8e
AaronW <zmtzwb@gmail.com>
2020-01-26 14:43:19 +0800

    f

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
-import (
-	"MiniVideo/database"
-	"MiniVideo/router"
-)
+import "MiniVideo/router"
 
 func main() {
 	router.InitRoute()
-	defer database.Mongo.Close()
+	//defer database.Mongo.Close()
 
 	//pwd, _ := utils.EncryptStringToPassword("123456")
 	//u := &models.User{
